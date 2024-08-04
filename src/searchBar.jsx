bbafcbe706636fb89d3a8f5b5913b83be28e58bf
achoyy <abigail.choy.08@gmail.com>
2024-08-04 05:25:23 -0400

    created search box
    needs positioning

--- src/searchBar.jsx
+++ src/searchBar.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './searchBar.css';
 
 function Search() { 
+    useEffect(()=> {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => { 
+            console.log(data); 
+            setData(data);
+            setFilterData(data);
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    const [data, setData] = useState([]);
+    const [filterData, setFilterData] = useState([]);
+    const handleFilter = (value) => { 
+        if (value === "") { 
+            setData([]);
+        }
+        else {
+            const res = filterData.filter(f => f.name.toLowerCase().includes(value.toLowerCase()));
+            setData(res);
+        } 
+    }
     return (
-        <div className='search'> 
-            <input type="text" placeholder='Search...'/> 
-        </div> 
+        <div className = 'search-top'>
+            <div className='search'> 
+                <input 
+                    type="text" 
+                    placeholder='Search...' 
+                    onChange={e => handleFilter(e.target.value)}
+                /> 
+            </div> 
+            <div className='search-result'>
+                {data.map((d, i) => (
+                    <div key={i}>
+                        {d.name}
+                    </div>
+                ))}
+            </div> 
+        </div>
     )
 }
 
