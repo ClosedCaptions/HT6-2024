02c1100ec755daa8d7108e988f6e59a5adf22bd6
achoyy <abigail.choy.08@gmail.com>
2024-08-04 03:58:53 -0400

    created search bar

---
+++ src/searchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './searchBar.css';
+
+function Search() { 
+    return (
+        <div className='search'> 
+            <input type="text" placeholder='Search...'/> 
+        </div> 
+    )
+}
+
+export default Search;  
---
+++ src/titleComp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TitleComp = () => {
+  return (
+    <p style={{ textAlign: 'left', paddingLeft: '10px' }}>App Name</p>
+  );
+};
+
+export default TitleComp;
