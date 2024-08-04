cd2a5e33aa730af544af34cf71245d002e8b660d
achoyy <abigail.choy.08@gmail.com>
2024-08-04 09:49:52 -0400

    maps work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
-// import logo from './logo.svg';
-import TitleComp from './titleComp';
-import './App.css';
-import Search from './searchBar'; 
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import React, { useState, useEffect } from 'react';
+import './App.css'; 
+import Search from './searchBar.jsx'; 
 
-function App() {
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '80vh',
+};
+
+const App = () => {
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+      );
+    }
+    else {
+      console.error('Error in gathering coordinates!');
+    }
+  }, []);
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyDza-zi_w7Y1nQvUbcPFj84fKtXuXyj_fo',
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  const center = {
+    lat: userLocation.latitude,
+    lng: userLocation.longitude
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <TitleComp /> 
-      </header>
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={20}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
       <header className = "bar"> 
-        <Search></Search>
+        <Search />
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/titleComp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TitleComp = () => {
-  return (
-    <p style={{ textAlign: 'left', paddingLeft: '10px' }}>App Name</p>
-  );
-};
-
-export default TitleComp;
